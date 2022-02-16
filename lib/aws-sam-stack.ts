fe3f5cfa7769f456c2ec62609225d66fb122cd76
non-97 <nonmonaka07@gmail.com>
2022-02-16 16:50:29 +0900

    initial commit

--- lib/aws-sam-stack.ts
+++ lib/aws-sam-stack.ts
@@ -1,16 +1,120 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  Fn,
+  Stack,
+  StackProps,
+  aws_s3 as s3,
+  aws_s3_deployment as s3deploy,
+  aws_logs as logs,
+  aws_iam as iam,
+  aws_sam as sam,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 export class AwsSamStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const stackUniqueId = Fn.select(2, Fn.split("/", this.stackId));
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsSamQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // S3 buckets to store AWS Step Function Workflow
+    const sfnWorkflowBucket = new s3.Bucket(this, "SfnWorkflowBucket", {
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      blockPublicAccess: new s3.BlockPublicAccess({
+        blockPublicAcls: true,
+        blockPublicPolicy: true,
+        ignorePublicAcls: true,
+        restrictPublicBuckets: true,
+      }),
+    });
+
+    // Deploy AWS Step Function Workflow
+    new s3deploy.BucketDeployment(this, "DeployFilesToSfnWorkflowBucket", {
+      sources: [
+        s3deploy.Source.asset("./src/stepFunctions/", {
+          exclude: [".DS_Store"],
+        }),
+      ],
+      destinationBucket: sfnWorkflowBucket,
+    });
+
+    // CloudWatch Logs for State Machine Logs
+    const stateMachineLogGroup = new logs.LogGroup(
+      this,
+      "StateMachineLogGroup",
+      {
+        logGroupName: `/aws/vendedlogs/states/CfnStateMachine-${stackUniqueId}-Logs`,
+        retention: logs.RetentionDays.TWO_WEEKS,
+      }
+    );
+
+    // IAM Role for State Machine
+    const stateMachineIamRole = new iam.Role(this, "StateMachineIamRole", {
+      assumedBy: new iam.ServicePrincipal("states.amazonaws.com"),
+      managedPolicies: [
+        new iam.ManagedPolicy(this, "CloudWatchLogsDeliveryFullAccessPolicy", {
+          statements: [
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              resources: ["*"],
+              actions: [
+                "logs:CreateLogDelivery",
+                "logs:GetLogDelivery",
+                "logs:UpdateLogDelivery",
+                "logs:DeleteLogDelivery",
+                "logs:ListLogDeliveries",
+                "logs:PutResourcePolicy",
+                "logs:DescribeResourcePolicies",
+                "logs:DescribeLogGroups",
+              ],
+            }),
+          ],
+        }),
+        new iam.ManagedPolicy(this, "XRayAccessPolicy", {
+          statements: [
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              resources: ["*"],
+              actions: [
+                "xray:PutTraceSegments",
+                "xray:PutTelemetryRecords",
+                "xray:GetSamplingRules",
+                "xray:GetSamplingTargets",
+              ],
+            }),
+          ],
+        }),
+      ],
+    });
+
+    // AWS Step Functions State Machine
+    new sam.CfnStateMachine(this, "CfnStateMachine", {
+      definitionSubstitutions: {
+        definitionSubstitutionsKey: "definitionSubstitutions",
+      },
+      definitionUri: {
+        bucket: sfnWorkflowBucket.bucketName,
+        key: "workflow.asl.json",
+      },
+      logging: {
+        destinations: [
+          {
+            cloudWatchLogsLogGroup: {
+              logGroupArn: stateMachineLogGroup.logGroupArn,
+            },
+          },
+        ],
+        includeExecutionData: true,
+        level: "ALL",
+      },
+      name: "CfnStateMachine",
+      role: stateMachineIamRole.roleArn,
+      tags: {
+        Name: "CfnStateMachine",
+      },
+      tracing: {
+        enabled: true,
+      },
+      type: "STANDARD",
+    });
   }
 }
